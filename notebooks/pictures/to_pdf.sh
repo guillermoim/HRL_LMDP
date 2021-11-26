eaacaf78614a3e1ddf930f16f1c22f766e07ee4b
Guillermo <infante@Guillermos-MacBook-Pro.local>
2021-11-26 13:31:05 +0100

    added results plots

--- notebooks/pictures/to_pdf.sh
+++
@@ -1,5 +0,0 @@
-pdftoppm -png -r 300 overall_grid_8_8_1.pdf overall_grid_8_8_1
-pdftoppm -png -r 300 overall_grid_5_5_1_3.pdf overall_grid_5_5_1_3
-pdftoppm -png -r 300 overall_grid_3_3_1.pdf overall_grid_3_3_1
-pdftoppm -png -r 300 overall_taxi_10_1.pdf overall_taxi_10_1
-pdftoppm -png -r 300 overall_taxi_5_1.pdf overall_taxi_5_1
